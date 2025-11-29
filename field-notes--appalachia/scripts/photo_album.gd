a0d6bd23256a41fb03d99a15715cb5dfe653cc97
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2025-11-28 20:31:49 -0500

    photo album complete
    works for six photos max

--- field-notes--appalachia/scripts/photo_album.gd
+++ field-notes--appalachia/scripts/photo_album.gd
@@ -1,26 +1,31 @@
 extends ColorRect
 
 @onready var grid_container: GridContainer = $GridContainer
-@onready var photo_1: TextureRect = $GridContainer/Photo1
-@onready var photo_2: TextureRect = $GridContainer/Photo2
-@onready var photo_3: TextureRect = $GridContainer/Photo3
+@onready var photo_1: TextureRect = $GridContainer/PhotoFrame/TextureRect
+@onready var photo_2: TextureRect = $GridContainer/PhotoFrame2/TextureRect
+@onready var photo_3: TextureRect = $GridContainer/PhotoFrame3/TextureRect
+@onready var photo_4: TextureRect = $GridContainer/PhotoFrame4/TextureRect
+@onready var photo_5: TextureRect = $GridContainer/PhotoFrame5/TextureRect
+@onready var photo_6: TextureRect = $GridContainer/PhotoFrame6/TextureRect
 
-var photo_album : Array = [photo_1, photo_2, photo_3]
-
-const PHOTO_FRAME = preload("uid://bf8e1x8j4k2h2")
+var idx = 0
+var photo_for_texture_change
 
 func _ready():
-	#for photo in Globals.photo_album:
-	#for photo in photo_album:
-	var image := Image.new()
-	image.load("user://1.png")
-	var tex := ImageTexture.new()
-	tex = ImageTexture.create_from_image(image)
-	photo_1.texture = tex
-	#var tex = ImageTexture.create_from_image(load("res://archive/wakatime/godotwaka.png"))
-	#var tex = ImageTexture.create_from_image(load("res://in_game_pics/1.png"))
-		#Globals.photo_album[photo]
-	 # Update the empty TextureRect
+	var frames_in_cont = grid_container.get_children()
+
+	for photo in Globals.photo_album:
+		var image := Image.new()
+		var photo_path = Globals.photo_album[idx]
+		image.load(str(photo_path))
+		var tex := ImageTexture.new()
+		tex = ImageTexture.create_from_image(image)
+		photo_for_texture_change = frames_in_cont[idx]
+		var texture_rect = photo_for_texture_change.get_child(1)
+		texture_rect.texture = tex
+		idx += 1 
+		print('success ' + str(idx))
+
 
 func _process(delta: float) -> void:
 	if (Input.is_action_just_pressed("debug")):
